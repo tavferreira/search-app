a599cb172894d1ea942339bc7797216cd4c9d91b
Tiago Ferreira <tiagoferreira@tuta.io>
2025-04-03 21:46:35 +0200

    chore: organize file structure

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { openLibraryApi } from '../features/search/api/openLibraryApi'
+
+export const store = configureStore({
+  reducer: {
+    [openLibraryApi.reducerPath]: openLibraryApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(openLibraryApi.middleware),
+})
+
+setupListeners(store.dispatch)
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
